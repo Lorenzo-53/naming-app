27128b43264da190504efe56d90f5ec6ea50aa4e
Lorenzo-53 <anshsrivstava835@gmail.com>
2026-01-19 02:34:07 +0530

    jus little changes

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,1 @@
-import 'package:flutter/material.dart';
 
-class HomePagee extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Home Page")),
-      body: Center(child: Text("hii", style: TextStyle(fontSize: 40))),
-    );
-  }
-}
